012fd729ee9f0fc1a0838e7594ed8bd077f1901a
sanchuan <sanchuan@ubuntu>
2013-12-10 11:54:21 +0800

    implement a message console

--- AppInsightEclipse/src/cn/ac/iscas/appinsighteclipse/Activator.java
+++ AppInsightEclipse/src/cn/ac/iscas/appinsighteclipse/Activator.java
@@ -1,4 +1,4 @@
-package appinsighteclipse;
+package cn.ac.iscas.appinsighteclipse;
 
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
--- AppInsightEclipse/src/cn/ac/iscas/appinsighteclipse/popup/actions/NewAction.java
+++ AppInsightEclipse/src/cn/ac/iscas/appinsighteclipse/popup/actions/NewAction.java
@@ -6,10 +6,26 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleManager;
+import org.eclipse.ui.console.MessageConsole;
+import org.eclipse.ui.console.MessageConsoleStream;
+import org.eclipse.core.runtime.ILog;
+import org.eclipse.core.runtime.Status;
+
+import cn.ac.iscas.appinsighteclipse.Activator;
 
 public class NewAction implements IObjectActionDelegate {
 
 	private Shell shell;
+	
+	ILog logger;
+	ConsolePlugin consolePlugin;
+	IConsoleManager consoleManager;
+	IConsole[] consoles;
+	MessageConsole appInsightConsole;
+	MessageConsoleStream appInsightConsoleStream;
 	
 	/**
 	 * Constructor for Action1.
@@ -33,6 +49,37 @@ public class NewAction implements IObjectActionDelegate {
 			shell,
 			"AppInsightEclipse",
 			"AppInsightEclipse Instrument was executed.");
+		
+		
+		logger = Activator.getDefault().getLog();
+		logger.log(new Status(Status.INFO,Activator.PLUGIN_ID,Status.OK,
+				"hi",null));
+		
+		
+		consolePlugin = ConsolePlugin.getDefault();
+		consoleManager = consolePlugin.getConsoleManager();
+		consoles = consoleManager.getConsoles();
+		
+		logger.log(new Status(Status.INFO,Activator.PLUGIN_ID,Status.OK,
+				String.valueOf(consoles.length),null));
+		
+		for(int i = 0;i<consoles.length;i++)
+		{
+			logger.log(new Status(Status.INFO,Activator.PLUGIN_ID,Status.OK,
+				consoles[i].getName(),null));
+			if(consoles[i].getName().equals("AppInsightConsole"))
+				appInsightConsole = (MessageConsole)consoles[i];
+		}
+		appInsightConsole = new MessageConsole("AppInsightConsole",null);
+		consoleManager.addConsoles(new IConsole[]{appInsightConsole});
+		
+		appInsightConsoleStream = appInsightConsole.newMessageStream();
+		appInsightConsoleStream.println("hello, world");
+		
+		
+		
+		
+		
 	}
 
 	/**
